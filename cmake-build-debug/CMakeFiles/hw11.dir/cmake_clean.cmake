04e10398ca0a7988d997db31664af8540fe8147f
elfsa-code <elfsaikou@gmail.com>
2023-05-06 21:21:55 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/hw11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw11.dir/src/apps/hw11.cpp.o"
+  "hw11"
+  "hw11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/include/ORB/global_defination/global_defination.h
@@ -0,0 +1,10 @@
+#ifndef ORB_GLOBAL_DEFINATION_H_IN_
+#define ORB_GLOBAL_DEFINATION_H_IN_
+
+#include <string>
+
+namespace ORB {
+    const std::string WORK_SPACE_PATH = "/home/elfsa/hw-day11/hw-day11";
+}
+
+#endif
